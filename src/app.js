ff3b5aa847d433fe70dabefb4d55774a94b6de7d
Вячеслав Абаимов <rickmoralis@MacBook-Air-Vaceslav.local>
2020-10-28 12:21:33 +0300

    init:start-project files

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import classnames from "classnames";
+import s from "./app.modules.scss";
+import "./custom.css";
+
+const App = () => {
+  return <div className={classnames(s.header, "color")}>koko</div>;
+};
+
+export default App;
